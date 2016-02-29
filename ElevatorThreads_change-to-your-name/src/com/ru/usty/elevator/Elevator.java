2278eb269c5a763842dbd6b11270f157285d9d82
Oddur Carl Thorarensen <oddurc@gmail.com>
2016-02-29 22:36:29 +0000

    Lagaði villur

--- ElevatorThreads_change-to-your-name/src/com/ru/usty/elevator/Elevator.java
+++ ElevatorThreads_change-to-your-name/src/com/ru/usty/elevator/Elevator.java
@@ -1,14 +1,14 @@
 package com.ru.usty.elevator;
 
 public class Elevator implements Runnable {
-    private int peopleCount;
+
     @Override
     public void run() {
-        if(ElevatorScene.elevatorsMayDie){
+        if(ElevatorScene.elevatorMayDie){
             return;
         }
         while(true) {
-            ElevatorScene.semaphore1.release(); // signal
+            ElevatorScene.floorQueue.release(); // signal
         }
     }
 }
